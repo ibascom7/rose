498df1d6112c9da1f2a388f3e5af8ac806d738cf
Isaiah Bascom <bascomisaiah@gmail.com>
2025-08-11 20:09:11 -0500

    prod config

--- config/prod.exs
+++ config/prod.exs
@@ -18,3 +18,8 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :rose, Rose.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: false
